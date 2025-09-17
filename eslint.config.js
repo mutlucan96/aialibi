b2f03ec8fea7e608327d9e839e3360c8f728ce1f
mutlu <mtlcnylmz@gmail.com>
2025-09-17 09:35:10 +0200

    add eslint and firebase

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginJsdoc from 'eslint-plugin-jsdoc'
+
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  pluginJsdoc.configs['flat/recommended'],
+  skipFormatting,
+])

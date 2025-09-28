ae93b0ea6500241ee4b7d27d21767682cb18e9d2
mutlu <mtlcnylmz@gmail.com>
2025-09-28 17:57:15 +0200

    lobby style

--- src/components/PresenterLobby.vue
+++ src/components/PresenterLobby.vue
@@ -1,22 +1,23 @@
 <template>
   <v-row align="center" justify="center">
     <!-- Left Column: Game Code and QR Code -->
-    <v-col cols="12" md="6" class="text-center">
+    <v-col cols="8" md="6" class="text-center">
       <p class="text-h6 text-center mb-4">Waiting for the host to start the game...</p>
       <h1 class="text-h3 mb-4">Join the Game!</h1>
       <div class="text-h1 font-weight-bold mb-8">
         {{ gameId }}
       </div>
-      <p class="ma-4 text-h6">Scan to join</p>
+      <p class="ma-4 text-h5">Scan to join</p>
       <div class="d-flex justify-center">
         <qrcode-vue :value="joinGameUrl" :size="200" level="H"></qrcode-vue>
       </div>
-      <p class="mt-2 text-body-1">
+      <p class="mt-2 text-h5">
         <a :href="joinGameUrl">{{ joinGameUrl }}</a>
       </p>
     </v-col>
-
-    <TeamList :game="game" />
+    <v-col cols="4" md="4">
+      <TeamList :game="game" />
+    </v-col>
   </v-row>
 </template>
 
@@ -40,7 +41,6 @@ const props = defineProps({
     required: true,
   },
 })
-
 
 const joinGameUrl = computed(() => {
   return `${window.location.origin}/game/${props.gameId}`

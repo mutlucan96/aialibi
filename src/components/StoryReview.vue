c970909fefb2c6e7ff840ebb4e15ec40b6bd4d22
mutlu <mtlcnylmz@gmail.com>
2025-09-29 09:35:45 +0200

    story style

--- src/components/StoryReview.vue
+++ src/components/StoryReview.vue
@@ -1,51 +1,44 @@
 <template>
-  <v-container>
-    <!-- Crime Description -->
-    <v-card class="mb-6">
-      <v-card-title>The Crime</v-card-title>
-      <v-card-text>{{ caseFile.crime }}</v-card-text>
-      <v-card-actions v-if="gameSettings.mode === 'race'">
-        <v-btn variant="text" size="small" @click="$emit('update:showSolution', !showSolution)">
-          {{ showSolution ? 'Hide solution' : 'Show solution...' }}
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+  <!-- Crime Description -->
+  <v-card class="mb-2">
+    <v-card-title>The Crime</v-card-title>
+    <v-card-text>{{ caseFile.crime }}</v-card-text>
+    <v-card-actions v-if="gameSettings.mode === 'race'">
+      <v-btn variant="text" size="small" @click="$emit('update:showSolution', !showSolution)">
+        {{ showSolution ? 'Hide solution' : 'Show solution...' }}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 
-    <!-- Solution Display (Race Mode Only) -->
-    <v-expand-transition>
-      <div v-if="showSolution && gameSettings.mode === 'race'">
-        <v-card class="mb-6" color="red-lighten-5">
-          <v-card-title>Top Secret: The Solution</v-card-title>
-          <v-card-text>
-            <p><strong>Culprit:</strong> {{ caseFile.culprit }}</p>
-            <p><strong>Motive:</strong> {{ caseFile.motive }}</p>
-          </v-card-text>
-        </v-card>
-      </div>
-    </v-expand-transition>
+  <!-- Solution Display (Race Mode Only) -->
+  <v-expand-transition>
+    <div v-if="showSolution && gameSettings.mode === 'race'">
+      <v-card class="mb-2" color="red-lighten-5">
+        <v-card-title>Top Secret: The Solution</v-card-title>
+        <v-card-text>
+          <p><strong>Culprit:</strong> {{ caseFile.culprit }}</p>
+          <p><strong>Motive:</strong> {{ caseFile.motive }}</p>
+        </v-card-text>
+      </v-card>
+    </div>
+  </v-expand-transition>
 
-    <!-- Witness List -->
-    <v-row>
-      <v-col v-for="(witness, index) in localWitnesses" :key="index" cols="12" md="6">
-        <v-card>
-          <div class="d-flex align-center pa-4">
-            <v-avatar
-              v-if="witness.imageUrl"
-              :image="witness.imageUrl"
-              size="96"
-              class="mr-4"
-            ></v-avatar>
-            <v-avatar v-else size="96" class="mr-4" color="grey-lighten-1">
-              <span class="text-h3">{{ witness.name.charAt(0) }}</span>
-            </v-avatar>
-            <v-card-title class="pl-0">{{ witness.name }}</v-card-title>
-          </div>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <!-- Witness List -->
+  <v-row class="pa-2">
+    <v-col v-for="(witness, index) in localWitnesses" :key="index" cols="12" md="6" class="pa-1">
+      <v-card>
+        <div class="d-flex align-center pa-1">
+          <v-avatar v-if="witness.imageUrl" :image="witness.imageUrl" size="96" class=""></v-avatar>
+          <v-avatar v-else size="96" class="" color="grey-lighten-1">
+            <span class="text-h3">{{ witness.name.charAt(0) }}</span>
+          </v-avatar>
+          <v-card-title class="pl-4">{{ witness.name }}</v-card-title>
+        </div>
+      </v-card>
+    </v-col>
+  </v-row>
   <!-- Control Buttons -->
-  <v-footer absolute class="d-flex flex-wrap justify-center pa-2 ga-2">
+  <v-card absolute class="d-flex flex-wrap justify-center pa-2 mt-2" rounded="lg">
     <v-btn
       v-if="!imagesGenerated"
       @click="$emit('generate-images')"
@@ -56,7 +49,7 @@
       Generate Images
     </v-btn>
     <v-btn @click="$emit('start-game')" color="primary" size="large"> Start Game </v-btn>
-  </v-footer>
+  </v-card>
 </template>
 
 <script setup>

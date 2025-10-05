c4e62acdf6c46ba71bb2e18264b62416dce6c88c
mutlu <mtlcnylmz@gmail.com>
2025-10-05 13:45:56 +0200

    drop logs on build

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,13 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true }),
-  ],
+  plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
   },
 })
